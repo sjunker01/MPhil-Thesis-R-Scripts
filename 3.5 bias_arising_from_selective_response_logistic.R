b0f0895590d4239d058f0c16fee6b2572f107e41
sjunker01 <48058698+sjunker01@users.noreply.github.com>
2019-08-06 14:52:21 +0100

    Update 3.5 bias_arising_from_selective_response_logistic.R

--- 3.5 bias_arising_from_selective_response_logistic.R
+++ 3.5 bias_arising_from_selective_response_logistic.R
@@ -14,24 +14,11 @@ round_df <- function(x, digits) {
   x
 }
 
-wide <- read_csv("data_input(4)/mindful_drop(FUbias).csv",
-                 col_types = cols(arm = col_factor(levels = c("intervention", "control", "MMJ")),
-                                  gender = col_factor(levels = c("Female", "Male"))))
-wide <- wide %>% rename(row = X1)
-
-# Bin ethnicity
-wide$ethnicity <- with(wide, ifelse(ethnicity == "White", "White", "Non-White"))
-
-# Rename arm
-wide <- wide %>% 
-  mutate(intake = ifelse(arm == "intervention", "Intake 1",
-                         ifelse(arm == "control", "Intake 2",
-                                ifelse(arm == "MMJ", "Intake 3", NA))))
-wide$intake <- factor(wide$intake, levels = c("Intake 1", "Intake 2", "Intake 3"))
+### Read in data
+wide <- read_csv("data_input(4)/mindful_drop(FUbias).csv")
 
 
-
-### Create color palette (darkest)
+### Create color palette
 cols <- c("#e47e32", "#ff9e1f", "#ae9764", "#719f8d", "#509094", "#d2c078")
 col1 <- cols[1]
 col2 <- cols[2]
@@ -42,17 +29,16 @@ col6 <- cols[6]
 
 
 
-### Attendance on answering post-int.
+##### Stats
 
 # Create variables stating whether or not people have answered the post-intervention questionnaire.
 wide <- wide %>% mutate(answered_post = !is.na(core_post)) %>% 
   mutate(answered_post = ifelse(answered_post == TRUE, 1, 0))
-wide$answered_post <- as.numeric(wide$answered_post)
+wide$answered_post <- as.numeric(wide$answered_post) # Converd to numbers
 
 # Create dataset with only needed variables
 wide_att <- wide %>% select(sessions_attended, answered_post, intake, age, disability, gender,
                             core_base)
-
 
 
 # Fit overall model, accounting for known confounding factors, excluding disability (unadjusted)
@@ -60,15 +46,16 @@ wide_att <- wide %>% select(sessions_attended, answered_post, intake, age, disab
 wide_unadj <- wide_att %>% # n = 429
   filter(!is.na(age) & !is.na(gender))
 
+# Model (basic adjustment)
 glm.unadj <- glm(answered_post ~ sessions_attended + age + gender + intake,
                data = wide_unadj, family = binomial)
 
-
+# Fit model adjusted for disability
 # Create dataset
 wide_adj <- wide_att %>% # n = 422
   filter(!is.na(age) & !is.na(gender) & !is.na(disability) & !is.na(core_base))
 
-# Fit overall model, adjusted (accounting for disability)
+# Model (adjusted for disability)
 glm.adj <- glm(answered_post ~ sessions_attended + age + gender + intake + disability +
                  core_base,
                data = wide_adj, family = binomial)
@@ -82,6 +69,7 @@ fit.gamlss <- gamlss(answered_post ~ sessions_attended + age + gender + intake +
 plot(fit.gamlss)
 
 
+# Call summary on the unadjusted and adjusted models.
 summary(glm.unadj)
 summary(glm.adj)
 # Interpretation: For every unit of change in x, the log odds of answering the questionnaire vs.
@@ -89,19 +77,13 @@ summary(glm.adj)
 
 
 
-### Extract data
-
+## Extract data
 # Of unadjusted model
 stargazer(cbind(Estimate = coef(glm.unadj), Std.Error = coef(summary(glm.unadj))[,2],
                 z.value = coef(summary(glm.unadj))[,3], OR = exp(coef(glm.unadj)),
                 exp(confint(glm.unadj)),
                 p_value = coef(summary(glm.unadj))[,4]), type = "text", style = "qje", digits = 3)
 
-output.unadj <- round_df(cbind(Estimate = coef(glm.unadj),OR = exp(coef(glm.unadj)),
-                               exp(confint(glm.unadj)),
-                               p_value = coef(summary(glm.unadj))[,4]), 3)
-
-write.csv(output.unadj, file = "model_output/4_answeredpost_unadj.csv")
 
 # Of adjusted model
 stargazer(cbind(Estimate = coef(glm.adj), Std.Error = coef(summary(glm.adj))[,2],
@@ -109,26 +91,7 @@ stargazer(cbind(Estimate = coef(glm.adj), Std.Error = coef(summary(glm.adj))[,2]
                 exp(confint(glm.adj)),
                 p_value = coef(summary(glm.adj))[,4]), type = "text", style = "qje", digits = 3)
 
-output.adj <- round_df(cbind(Estimate = coef(glm.adj),OR = exp(coef(glm.adj)),
-                             exp(confint(glm.adj)),
-                             p_value = coef(summary(glm.adj))[,4]), 3)
 
-write.csv(output.adj, file = "model_output/4_answeredpost_adj.csv")
-
-
-
-
-### Let us assess the overall effect / significance of arm on the model.
-
-## Using a model comparison
-# Update the glm model dropping arm
-glm.noarm <- update(glm.att, . ~ . - arm)
-# Test model differences with chi square test
-anova(glm.att, glm.noarm, test="Chisq") # p < 2.2e-16
-
-## Using the Wald Test
-wald.test(b = coef(glm.att), Sigma = vcov(glm.att), Terms = 3:4)
-# Both tell us p is very small.
 
 
 ### Let us assess the effect on the model when disability and core at baseline are removed.
@@ -141,7 +104,8 @@ anova(glm.adj, glm.noadj, test="Chisq") # p = 0.36
 
 ## Using the Wald Test
 wald.test(b = coef(glm.adj), Sigma = vcov(glm.adj), Terms = 7:8) p = 0.37
-# Disability and core_base did not significantly change the model
+# Disability and core_base did not significantly change the model. But more important is whether the
+# other variable estimates change, and they do not.
 
 ## Check whether disability and core improve the model
 glm.nodis <- update(glm.adj, . ~ . -disability)
@@ -153,17 +117,9 @@ AIC(glm.nocore)
 AIC(glm.noadj)
 
 
-## Extra exploration: How to test the difference between two categorical options
-# Difference between intervention and MMJ arm
-l <- cbind(0, 0, 1, -1, 0, 0, 0)
-wald.test(b = coef(glm.att), Sigma = vcov(glm.att), L = l) # p = 1.1e-16, shows exactly what
-# summary() shows when changing the order of the arm options
-
-
-
 ### Use predicted probabilities to help us understand the model.
 
-# Let's hold all variables by their means / by the factor which is used most and let arm differ.
+## Let's hold all variables by their means / by the factor which is used most and let arm differ.
 summary(wide_att)
 test1 <- with(wide_att, data.frame(sessions_attended = mean(sessions_attended), age = mean(age, na.rm = TRUE),
                                    disability = FALSE, gender = "Female", arm = c("control", "MMJ",
@@ -174,7 +130,7 @@ test1
 # 90% chance of answering in intervention group with the same other variables.
 
 
-# Let's hold all variables by their means / by the factor which is used most and let sessions.
+## Let's hold all variables by their means / by the factor which is used most and let sessions differ.
 test2 <- with(wide_att, data.frame(sessions_attended = c(0,1,2,3,4,5,6,7,8), age = mean(age, na.rm = TRUE),
                                    disability = FALSE, gender = "Female", arm = "intervention"))
 test2$answerP <- predict(glm.att, newdata = test2, type = "response")
@@ -184,7 +140,8 @@ test2
 
 
 
-##### Let's predict and visualize
+## Let's hold all variables by their means / by the factor which is used most and let sessions and
+## arm differ.
 test3 <- with(wide_att, data.frame(sessions_attended = rep(seq(from = 0, to = 8, length.out = 20),4),
                                    age = mean(age, na.rm = TRUE), disability = FALSE, gender = "Female",
                                    arm = factor(rep(c("control", "MMJ", "intervention"), each = 240))))
@@ -198,8 +155,7 @@ testdata <- within(testdata, {
 })
 
 
-
-# Plot for non-disabled females of the mean age (23.7)
+# Visualize for non-disabled females of the mean age (23.7)
 ggplot(testdata, aes(x = sessions_attended, y = PredictedProb)) +
   geom_ribbon(aes(ymin = LL, ymax = UL, fill = Intake), alpha = 0.2) +
   geom_line(aes(colour = Intake), size = 1) +
@@ -215,11 +171,11 @@ ggplot(testdata, aes(x = sessions_attended, y = PredictedProb)) +
   )
 
 
-##### Have in one plot with only 2 predictors which are displayed
+
+##### Plotting
 
 # Create a model which only contains the significant predictors, which we will also
 # visualize in the plot
-
 glm.simple <- glm(answered_post ~ intake + sessions_attended, data = wide_att, family = "binomial")
 summary(glm.simple)
 
@@ -254,45 +210,6 @@ ggplot(plot.simple, aes(x = sessions_attended, y = PredictedProb)) +
     panel.grid.minor = element_blank()
   ) -> a
 
+# Save
 ggsave(a, filename = "plots_thesis/4_sess_on_prob.png", device = "png",
        width = 7.5, height= 5, units = "in")
-
-##### CHECK WHETHER MY MODEL IS GOOD
-
-# Generate a random dataset (2/3 of participants) from which to make the model.
-(nrow(wide_att))/3*2 # 339 is 2/3 of the dataset.
-rows <- sample(1:509, 339, replace = FALSE)
-dataset1 <- wide_att %>% filter(row %in% rows)
-dataset2 <- wide_att %>% filter(!(row %in% rows))
-
-# Now run a logistic regression on the first group and use it to make a prediction
-glm.subset <- glm(answered_post ~ sessions_attended + arm, data = dataset1, family = binomial)
-summary(glm.subset)
-
-# Make a vector of fitted probabilities
-glm.probs <- predict(glm.subset, type = "response")
-glm.probs[1:20]
-# Make a prediction what participants would have taken if the model was perfect
-glm.pred <- ifelse(glm.probs > 0.5, "1", "0")
-table(glm.pred, dataset1$answered_post)
-mean(glm.pred == dataset1$answered_post)
-# In 81% of cases this prediction is correct (for the same dataset).
-
-# Now let's check for the other randomly created dataset.
-dataset2.reduced <- dataset2 %>% dplyr::select(arm, sessions_attended)
-glm.probs <- predict(glm.noarm, newdata = dataset2.reduced, tpye = "response")
-glm.pred <- ifelse(glm.probs > 0.5, "1", "0")
-table(glm.pred, dataset2$answered_post)
-mean(glm.pred == dataset2$answered_post)
-# For my other data, the model I have produced is correct in 61% of the time.
-# Gives me an idea of how much my model would be applicable to the general population.
-
-
-# Log likelihood ratio test
-LLR = -2 * (glm.att$null.deviance - glm.att$deviance)
-pchisq(LLR, 2, lower.tail = FALSE)
-# Gives 1.... Help?
-
-
-
-
